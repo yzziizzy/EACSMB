979a5acf476f70391127786d8656e36e2a89c5eb
Izzy <yzziizzy@gmail.com>
2015-11-02 22:51:50 -0800

    use a texture that's in the repo ;)

--- src/map.c
+++ src/map.c
@@ -51,7 +51,7 @@ void initTerrain() {
 	};
 	
 	
-	cnoise = loadBitmapTexture("./assets/textures/grass texture3.png");
+	cnoise = loadBitmapTexture("./assets/textures/grass_texture-256.png");
 	
 	
 	glerr("clearing before terrain program load");
