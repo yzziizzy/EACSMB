95f4a6b692b2ff51fede16ad35de8e2a0df63964
Izzy <yzziizzy@gmail.com>
2016-06-27 23:35:59 -0700

    calculus only works when you do it right :P

--- src/shaders/projRoad.glsl
+++ src/shaders/projRoad.glsl
@@ -24,23 +24,44 @@ float quad_bezier(float a, float b, float c, float t, float omt) {
 	return (omt * omt * a) + (2 * omt * t * b) + (t * t * c);
 }
 
+float quad_bezier_dt(float a, float b, float c, float t, float omt) {
+	return (2 * omt * (b - a)) + (2 * t * (c - b));
+}
+
 void main() {
+	// y is the curve parameter
 	float t = pos_tex_in.y; 
 	float omt = 1 - t;
+	
+	// x is the length to scale the normal vector;
+	// the sign of x indicates which side this vertex is for
+	float hwidth = pos_tex_in.x;
 	
 	const vec2 cp0 = cp02_in.xy;
 	const vec2 cp1 = cp1_in;
 	const vec2 cp2 = cp02_in.zw;
 	
+	// this is the actual vertex location
 	vec4 spline = vec4(
 		quad_bezier(cp0.x, cp1.x, cp2.x, t, omt),
 		quad_bezier(cp0.y, cp1.y, cp2.y, t, omt),
 		0, 1);
 		
+	// tangent vector of the curve
+	vec2 sdt = normalize(vec2(
+		quad_bezier_dt(cp0.x, cp1.x, cp2.x, t, omt),
+		quad_bezier_dt(cp0.y, cp1.y, cp2.y, t, omt)));
 	
+	// normal vector from the curve
+	sdt = sdt.yx;
+	sdt.x *= -1;
+	
+	sdt *= -hwidth;
+	
+	//spline.x += hwidth;
 
 //	gl_Position = (mProj * mView * mModel) * vec4(pos_tex_in.xy, 0, 1);
-	gl_Position = (mProj * mView * mModel) * (vec4(pos_tex_in.xy, 0, 1) + spline);
+	gl_Position = (mProj * mView * mModel) * (spline + vec4(sdt.xy, 0, 1));
 	vs_norm =  normalize(vec4(1,1,1,0));
 	vs_tex = pos_tex_in.zw;
 }
