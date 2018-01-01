21ca60fef4f203f7c6b4bfafd07d1a1c11c831f1
Izzy <yzziizzy@gmail.com>
2017-12-31 23:00:04 -0700

    cram in some unfinished code before the year ends

---
+++ src/builder/render.c
@@ -0,0 +1,201 @@
+
+
+
+
+#include "render.h" 
+
+
+
+
+
+
+#include "shader.h"
+
+
+
+GLuint quadVAO, quadVBO;
+
+
+
+
+
+void init_Builder() {
+	
+	float vertices[] = {
+		-1.0, -1.0, 0.0,
+		-1.0, 1.0, 0.0,
+		1.0, -1.0, 0.0,
+		1.0, 1.0, 0.0
+	};
+	
+	glGenVertexArrays(1, &quadVAO);
+	glBindVertexArray(quadVAO);
+	
+	glGenBuffers(1, &quadVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, 0);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+}
+
+
+
+void draw_quad(void* data, RenderParams* rp) {
+	
+	glBindVertexArray(quadVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glexit("quad vbo");
+
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	glexit("quad draw");
+} 
+
+
+
+
+enum {
+	DIFFUSE = 0,
+	NORMAL,
+	LIGHTING,
+	DEPTH,
+	OUTPUT
+}
+
+
+
+
+
+void Builder_initFBOs(BuilderPiperline* bp) {
+	
+	// diffuse, normal, lighting, depth, output, NULL
+	FBOTexConfig* texcfg = calloc(1, 6 * sizeof(*texcfg)); 
+	
+	texcfg[DIFFUSE].internalType = GL_RGB;
+	texcfg[DIFFUSE].format = GL_RGB;
+	texcfg[DIFFUSE].size = GL_UNSIGNED_BYTE;
+	texcfg[NORMAL].internalType = GL_RGB;
+	texcfg[NORMAL].format = GL_RGB;
+	texcfg[NORMAL].size = GL_UNSIGNED_BYTE;
+	texcfg[LIGHTING].internalType = GL_RGB16F;
+	texcfg[LIGHTING].format = GL_RGB;
+	texcfg[LIGHTING].size = GL_HALF_FLOAT;
+	texcfg[DEPTH].internalType = GL_DEPTH_COMPONENT32;
+	texcfg[DEPTH].format = GL_DEPTH_COMPONENT;
+	texcfg[DEPTH].size = GL_FLOAT;
+	texcfg[OUTPUT].internalType = GL_RGBA8;
+	texcfg[OUTPUT].format = GL_RGBA;
+	texcfg[OUTPUT].size = GL_UNSIGNED_BYTE;
+	
+	
+	// TODO: clean up old backing textures if existing
+	
+	bp->backingTextures = initFBOTextures(bp->viewSz.x, bp->viewSz.y, texcfg);
+	
+	
+	
+	FBOConfig gbufConf[] = {
+		{GL_COLOR_ATTACHMENT0, bp->backingTextures[DIFFUSE] },
+		{GL_COLOR_ATTACHMENT1, bp->backingTextures[NORMAL] },
+		{GL_COLOR_ATTACHMENT2, bp->backingTextures[LIGHTING] },
+		{GL_DEPTH_ATTACHMENT, bp->backingTextures[DEPTH] },
+		{0,0}
+	};
+	
+	initFBO(&bp->gbuf, gbufConf);
+	
+	
+	
+}
+
+
+
+void BuilderPass_init(BuilderPass* bp, ShaderProgram* prog) {
+	
+	// these will generate harmless errors if the uniform is not found
+	bp->diffuseUL = glGetUniformLocation(prog->id, "sDiffuse");
+	bp->normalsUL = glGetUniformLocation(prog->id, "sNormals");
+	bp->lightingUL = glGetUniformLocation(prog->id, "sLighting");
+	bp->depthUL = glGetUniformLocation(prog->id, "sDepth");
+	glerr("harmless");
+}
+
+
+
+void BuilderPipeline_renderAll(BuilderPiperline* bp, RenderParams* rp) {
+	
+	
+	for(int i = 0; i < VEC_LEN(&bp->passes); i++) {
+		BuilderPass* pass = VEC_ITEM(&bp->passes, i);
+		ShaderProgam* prog = pass->prog;
+		
+		glBindFramebuffer(GL_FRAMEBUFFER, bp->fbos[pass->fboIndex]);
+		
+		int clear = 0;
+		if(pass->clearColor) clear |= GL_COLOR_BUFFER_BIT;
+		if(pass->clearDepth) clear |= GL_DEPTH_BUFFER_BIT;
+		if(clear) glClear(clear);
+		
+		// TODO: ensure the backin textures are not bound to an active fbo
+		if(bp->diffuseUL != -1) {
+			glActiveTexture(GL_TEXTURE0 + 10);
+			glBindTexture(GL_TEXTURE_2D, bp->backingTexturesp[DIFFUSE]);
+			glProgramUniform1i(prog->id, bp->diffuseUL, 10);
+		}
+		
+		if(bp->normalsUL != -1) {
+			glActiveTexture(GL_TEXTURE0 + 11);
+			glBindTexture(GL_TEXTURE_2D, bp->backingTexturesp[NORMALS]);
+			glProgramUniform1i(prog->id, bp->normalsUL, 11);
+		}
+		
+		if(bp->lightingUL != -1) {
+			glActiveTexture(GL_TEXTURE0 + 12);
+			glBindTexture(GL_TEXTURE_2D, bp->backingTexturesp[LIGHTING]);
+			glProgramUniform1i(prog->id, bp->lightingUL, 12);
+		}
+		
+		if(bp->depthUL != -1) {
+			glActiveTexture(GL_TEXTURE0 + 13);
+			glBindTexture(GL_TEXTURE_2D, bp->backingTexturesp[DEPTH]);
+			glProgramUniform1i(prog->id, bp->depthUL, 13);
+		}
+		
+		
+		//glDepthFunc(GL_LEQUAL);
+// 		glDepthMask(GL_TRUE);
+		BuilderPass_renderAll(pass, rp);
+		
+		
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+}
+
+
+
+void BuilderPass_renderAll(BuilderPass* bp, RenderParams* rp) {
+	
+	int i;
+	
+	
+	// rebind fbos, textures, etc
+	
+	for(i = 0; i < VEC_LEN(&bp->renderables); i++) {
+		BuilderRenderable* r = &VEC_ITEM(&bp->renderables, i);
+		r->render(r->data, rp;
+	}
+	
+}
+
+
+
+
+
+
+
+
---
+++ src/builder/render.h
@@ -0,0 +1,92 @@
+#ifndef __EACSMB_builder_render_h__
+#define __EACSMB_builder_render_h__
+
+
+
+
+
+
+
+
+#include "../meshBuilder.h" 
+#include "../dynamicMesh.h" 
+#include "../shader.h" 
+
+
+
+typedef struct RenderParams {
+	
+	Vector w_eyePos;
+	Vector w_eyeDir;
+	
+	Vector2i fboSize;
+	
+	
+	Matrix* mWorldView;
+	Matrix* mViewProj;
+	
+	
+	//time
+	
+} RenderParams;
+
+
+
+
+typedef struct BuilderRenderable {
+	void (*render)(void* data, RenderParams* rp);
+	void* data;
+	
+	//shader here?
+} BuilderRenderable;
+
+
+typedef struct BuilderPass {
+	
+	char clearColor;
+	char clearDepth;
+	
+	char fboIndex;
+	// fbo config, texture bindings, etc
+	
+	ShaderProgram* prog;
+	
+	GLuint diffuseUL;
+	GLuint normalsUL;
+	GLuint lightingUL;
+	GLuint depthUL;
+	
+	//FBOConfig* gbufConfig;
+	
+	VEC(BuilderRenderable) renderables;
+	
+} BuilderPass;
+
+
+
+
+
+
+typedef struct BuilderPipeline {
+	DynamicMeshManager dmm;
+	
+	Vector2i viewSz;
+	
+	// fbo's
+	GLuint* backingTextures;
+	GLuint gbuf;
+	
+	Framebuffer fbos[2];
+	
+	VEC(BuilderPass*) passes;
+	
+} BuilderPipeline;
+
+
+
+void BuilderPipeline_renderAll(BuilderPiperline* bp, RenderParams* rp);
+
+
+
+
+#endif // __EACSMB_builder_render_h__
