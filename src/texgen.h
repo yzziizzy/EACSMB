f9183f981f419a7440415d8f8ee0e320628b78f8
Izzy <yzziizzy@gmail.com>
2018-03-12 21:50:56 -0600

    just some files

---
+++ src/texgen.h
@@ -0,0 +1,29 @@
+#ifndef __EACSMB_texgen_h__
+#define __EACSMB_texgen_h__
+
+
+
+#include "common_math.h"
+#include "common_gl.h"
+
+
+#include "texture.h"
+#include "perlin.h"
+#include "opensimplex.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // __EACSMB_texgen_h__
