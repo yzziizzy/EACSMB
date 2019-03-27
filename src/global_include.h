40c98a1cd67b2ef08129ac5c26ee4ed2346e7553
Izzy <yzziizzy@gmail.com>
2019-03-27 09:35:11 -0600

    file missed in prior commit

---
+++ src/global_include.h
@@ -0,0 +1,22 @@
+
+
+// This file is automatically included in every translation unit by the build system
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <assert.h>
+
+
+#ifdef NDEBUG
+	#define DebugBreak() ((void)0)
+#else
+	// requires the compiler to use AT&T assembly
+	#define DebugBreak() __asm__("int %3")
+#endif
+
